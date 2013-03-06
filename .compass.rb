4ba9c06c8afe7cd85bd90c275fc8a607bbc9f2fe
Dean Browne <deanbrowne@gmail.com>
2013-03-05 20:48:09 -0800

    Finished scaffolding

---
+++ .compass.rb
@@ -0,0 +1,3 @@
+# Explicitly set the path to the fonts/ directory so it works from `yeoman server` and
+# `yeoman build`.  https://github.com/yeoman/yeoman/issues/419
+http_fonts_path='fonts'
---
+++ test/vendor/chai/chai.js
@@ -0,0 +1,1 @@
+../../../components/chai/chai.js
