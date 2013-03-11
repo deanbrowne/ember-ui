cfd1fc9222732f7fb4f4db496325e045ac46df79
Dean Browne <deanbrowne@gmail.com>
2013-03-11 12:31:44 -0700

    ClickableView handles actions using the 'action' and 'target' properties.

---
+++ app/scripts/demo/button-controller.js
@@ -0,0 +1,8 @@
+/**
+  Action handlers for button demos.
+*/
+Demo.ButtonController = Ember.Controller.extend({
+  alert: function() {
+    alert('Clicked');
+  }
+});
--- app/scripts/ui/view/view.js
+++ app/scripts/ui/view/view.js
@@ -4,6 +4,10 @@
   (https://developers.google.com/mobile/articles/fast_buttons).
 
   `View` serves as the superclass for all other views in Ember.UI.
+
+  @class View
+  @namespace Ember.UI
+  @extends Ember.UI.ClickableView
 */
 Ember.UI.View = Ember.UI.ClickableView.extend({
 });
