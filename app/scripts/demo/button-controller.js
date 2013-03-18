3f9146715f55c1863680b8818c948173840c9282
Dean Browne <deanbrowne@gmail.com>
2013-03-17 20:23:31 -0700

    Touch ups to the demo app

--- app/scripts/demo/button-controller.js
+++ app/scripts/demo/button-controller.js
@@ -2,6 +2,14 @@
   Action handlers for button demos.
 */
 Demo.ButtonController = Ember.Controller.extend({
+  clickCount: 0,
+
+  incrementClickCount: function() {
+    var clicks = this.get('clickCount');
+    clicks += 1;
+    this.set('clickCount', clicks);
+  },
+
   alert: function() {
     alert('Clicked');
   }
