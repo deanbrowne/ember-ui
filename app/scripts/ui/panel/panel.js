7f2da09b3b907312fa32ca4a5971d23cdda4357a
Dean Browne <deanbrowne@gmail.com>
2013-03-18 15:16:09 -0700

    Added more {{panel}} properties

--- app/scripts/ui/panel/panel.js
+++ app/scripts/ui/panel/panel.js
@@ -5,7 +5,7 @@
   auxilary panels for lists, etc.
 
   ```handlebars
-  {{#panel id="menu" style="reveal" width="240px"}}
+  {{#panel id="menu" ordering="below" origin="left" width="240px"}}
     {{! Menu contents }}
   {{/panel}}
   ```
@@ -17,13 +17,97 @@
 Ember.UI.Panel = Ember.UI.View.extend({
 
   /**
+    The type of panel:
+      * 'below' appears beneath other content (default)
+      * 'above' appears over other content
+
+    @property ordering
+    @type {'below'|'above'}
+  */
+  ordering: 'below',
+
+  /**
+    If the panel appears from "left" or "right" side of the screen.
+
+    @property origin
+    @type {'left'|'right'}
+  */
+  origin: 'left',
+
+  /**
+    The width of the panel as a CSS value like '320px' or '10%'.
+
+    @property width
+    @type {String}
+  */
+  width: '100%',
+
+
+  /**
+    Properties to surface attributes on the <div>.
+
+    @property attributeBindings
+    @type {Array[String]}
+    @override
+  */
+  attributeBindings: ['style'],
+
+  /**
     Apply basic panel CSS.
 
     @property classNames
     @type {Array[String]}
     @override
   */
-  classNames: ['ui-panel']
+  classNames: ['ui-panel'],
+
+  /**
+    Changing the `depth` property will append CSS classes.
+
+    @property classNameBindings
+    @type {Array[String]}
+    @override
+  */
+  classNameBindings: ['_orderingCss'],
+
+  /**
+    The `style` attribute used in addition to `classNames`.
+
+    @property style
+    @type {String}
+  */
+  style: function() {
+    var vals = [];
+    vals.pushObject('width:' + this.get('width'));
+    return vals.join(';');
+  }.property('width'),
+
+  _orderingCss: function() {
+    var ordering = this.get('ordering');
+    return 'ui-panel-' + ordering;
+  }.property('ordering'),
+
+
+  show: function() {
+    // Set the edge just off the screen.  This happens immediately and will not be animated.
+
+    // Animate the panel onto the screen.
+    // this.$.css({ translateX: "100px"; });
+
+  },
+
+  hide: function() {
+    // translate -10000,-10000
+  },
+
+  toggle: function() {
+
+  },
+
+  // Good enough?  Or need to listen to <body>?
+  resize: function() {
+
+  }
 
 });
 
